7635fb269625584ee382655ebd50b30623c62d8a
dalimedmrad <dalimohamed789@gmail.com>
2024-03-10 12:14:41 +0100

    first

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const dbConnect = require("./config/connectDB");
+
+dbConnect();
+
+// middleware routing body parse
+app.use(express.json());
+app.use("/api/contact", require("./routes/contacts"))
+
+
+
+
+
+app.get("/", (req, res) => {
+  res.send("Hello Worldaaaaaaa");
+});
+
+
+
+
+
+
+
+
+
+
+
+const PORT = process.env.PORT;
+app.listen(PORT, (err) =>
+  err ? console.log(err) : console.log("server is runnig")
+);
